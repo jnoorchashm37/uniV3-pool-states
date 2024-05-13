2ac6557e9f844ca5222076e33cd486e8c6701f0b
lagrange.eth <lagrange@sorellacapital.com>
2024-05-13 02:48:42 -0400

    spawned on rt

--- src/lib.rs
+++ src/lib.rs
@@ -55,8 +55,10 @@ async fn get_initial_pools(
         SELECT DISTINCT
             (toString(address), init_block)
         FROM ethereum.pools
-        WHERE protocol = 'Uniswap' AND protocol_subtype = 'V3' AND address = '0xcbcdf9626bc03e24f779434178a73a0b4bad62ed'
+        WHERE protocol = 'Uniswap' AND protocol_subtype = 'V3' LIMIT 10
     ";
+
+    //AND address = '0xcbcdf9626bc03e24f779434178a73a0b4bad62ed'
 
     let initial_pools: Vec<(String, u64)> = db.query_many(query, &()).await?;
 
