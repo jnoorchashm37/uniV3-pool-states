1c631c868b29110344b2285bd8385d24eb957d76
lagrange.eth <lagrange@sorellacapital.com>
2024-05-13 02:45:57 -0400

    spawned on rt

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,7 @@ pub async fn run(handle: Handle) -> eyre::Result<()> {
         .map(|p| {
             let this_handle = handle.clone();
             handle.clone().spawn_blocking(move || {
-                this_handle.block_on(PoolHandler::new(p, current_block, 1000))
+                this_handle.block_on(PoolHandler::new(p, current_block, 100))
             })
         })
         .collect::<FuturesUnordered<_>>();
