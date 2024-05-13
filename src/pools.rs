76c2d85eae94389e9b0047a55869d405928a609b
lagrange.eth <lagrange@sorellacapital.com>
2024-05-13 19:33:35 -0400

    remodded

--- src/pools.rs
+++ src/pools.rs
@@ -72,7 +72,11 @@ impl<'a> PoolCaller<'a> {
 
         info!(target: "uni-v3", "completed block {} for {} pools with {} bips", self.block_number, pools.len(), state.len());
 
-        self.db.insert_many::<UniV3PoolState>(&state).await?;
+        if !state.is_empty() {
+            println!("{:?}", state[0])
+        }
+
+        //self.db.insert_many::<UniV3PoolState>(&state).await?;
 
         Ok(())
     }
